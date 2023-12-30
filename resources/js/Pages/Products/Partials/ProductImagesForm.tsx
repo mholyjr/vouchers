052b03baa925882470bacbdb1d4b8f9be97df9f4
Martin Holý <m.holyjr@gmail.com>
2023-12-30 14:06:24 +0100

    Add: preview of selected image

--- resources/js/Pages/Products/Partials/ProductImagesForm.tsx
+++ resources/js/Pages/Products/Partials/ProductImagesForm.tsx
@@ -5,25 +5,40 @@ import { ImagePreview } from '@/Components/ImagePreview';
 import { Flex } from '@tremor/react';
 
 export const ProductImagesForm: React.FC<ProductFormProps> = ({ form }) => {
+  const [preview, setPreview] = React.useState('');
+
+  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files ? e.target.files[0] : null;
+    if (file) {
+      form.setData('image', file);
+
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setPreview(reader.result as string);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
   return (
     <>
       <div>
         <InputLabel htmlFor="image">Main Image</InputLabel>
-        <Flex justifyContent='start'>
-          {form?.data?.image && (
-            /* 
-          // @ts-ignore */
-            <ImagePreview image={{ src: form.data.image, alt: 'Main image' }} />
+        <Flex justifyContent="start">
+          {preview ? (
+            <ImagePreview image={{ src: preview, alt: 'Image preview' }} />
+          ) : (
+            form?.data?.image && (
+              <ImagePreview
+                //@ts-ignore
+                image={{ src: form.data.image, alt: 'Main image' }}
+              />
+            )
           )}
           <input
             type="file"
             id="image"
             accept="image/*"
-            onChange={e => {
-              if (e.target.files && e.target.files[0]) {
-                form.setData('image', e.target.files[0]);
-              }
-            }}
+            onChange={handleImageChange}
           />
         </Flex>
       </div>
